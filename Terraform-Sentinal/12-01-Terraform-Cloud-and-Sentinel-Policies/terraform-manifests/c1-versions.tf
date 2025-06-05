ae46b140156614342d901265e3f77e6c78ac1c2e
Uma Mahesh <adabala.um@gmail.com>
2025-06-05 12:57:39 +0530

    Fix: Sentinel

--- Terraform-Sentinal/12-01-Terraform-Cloud-and-Sentinel-Policies/terraform-manifests/c1-versions.tf
+++ Terraform-Sentinal/12-01-Terraform-Cloud-and-Sentinel-Policies/terraform-manifests/c1-versions.tf
@@ -1,6 +1,6 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.7" 
+  required_version = ">= 1.7"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,19 +8,22 @@ terraform {
     }
   }
   # Update backend information as Terraform Cloud
-  backend "remote" {
-    organization = "hcta-demo1"
+
+  cloud {
+
+    organization = "Terra-Prac-01"
 
     workspaces {
-      name = "sentinel-demo1"
+      name = "Terraform-sentinel"
     }
-  }  
+  }
+
 
 }
 
 # Provider Block
 provider "aws" {
-  region  = var.aws_region
+  region = var.aws_region
 }
 /*
 Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
